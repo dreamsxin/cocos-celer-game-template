381f6fce1deb4569c30868d3b464c96119e8dce2
eatdreamcat <meow.std@outlook.com>
2020-11-12 10:23:30 +0800

    节点被删除时清空touchid

--- Celer-Game/assets/Scripts/View/SingleTouchMediator.ts
+++ Celer-Game/assets/Scripts/View/SingleTouchMediator.ts
@@ -13,110 +13,120 @@ import BaseView from "./BaseView";
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class SingleTouchMediator<T extends cc.Component> extends cc.Component {
+export default class SingleTouchMediator<
+  T extends cc.Component
+> extends cc.Component {
+  public bind(view: T) {
+    this.view = view;
+  }
 
-    public bind(view: T) {
+  private view: T;
+  get View(): T {
+    console.assert(this.view != null, " view is null");
+    return this.view;
+  }
 
-        this.view = view;
+  private touchid: number = null;
+
+  onRegister() {
+    this.node.targetOff(this);
+    this.node["_onSetParent"] = this._onSetParent.bind(this);
+    this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.touchCancel, this);
+    this.node.on(cc.Node.EventType.TOUCH_END, this.touchEnd, this);
+    this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMove, this);
+    this.node.on(cc.Node.EventType.TOUCH_START, this.touchStart, this);
+  }
+
+  onUnregister() {
+    this.node.targetOff(this);
+  }
+
+  private touchStart(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      console.log(
+        this.node.name,
+        " touch start  touchid is different: ",
+        this.touchid,
+        event.getID()
+      );
+      event.stopPropagation();
+      return;
+    }
+    this.touchid = event.getID();
+
+    this.onTouchStart(event);
+  }
+
+  private touchMove(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      //console.log(this.node.name, " touch move  touchid is different! ");
+      event.stopPropagation();
+      return;
+    }
+    this.touchid = event.getID();
+    this.onTouchMove(event);
+  }
+
+  private touchEnd(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      console.log(
+        this.node.name,
+        " touch end  touchid is different: ",
+        this.touchid,
+        event.getID()
+      );
+      event.stopPropagation();
+      return;
     }
 
-    private view: T;
-    get View(): T {
-        console.assert(this.view != null, " view is null");
-        return this.view;
+    if (this.touchid == null) {
+      event.stopPropagation();
+      return;
     }
 
+    this.touchid = null;
+    this.onTouchEnd(event);
+    this.OnClick();
+  }
 
-    private touchid: number = null;
-
-    onRegister() {
-        this.node.targetOff(this);
-
-        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.touchCancel, this);
-        this.node.on(cc.Node.EventType.TOUCH_END, this.touchEnd, this);
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMove, this);
-        this.node.on(cc.Node.EventType.TOUCH_START, this.touchStart, this);
+  private touchCancel(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      console.log(
+        this.node.name,
+        " touch cancel  touchid is different: ",
+        this.touchid,
+        event.getID()
+      );
+      event.stopPropagation();
+      return;
     }
 
-    onUnregister() {
-
-        this.node.targetOff(this);
+    if (this.touchid == null) {
+      event.stopPropagation();
+      return;
     }
 
-    private touchStart(event: cc.Event.EventTouch) {
+    this.touchid = null;
+    this.onTouchCancel(event);
+  }
 
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            console.log(this.node.name, " touch start  touchid is different: ", this.touchid, event.getID());
-            event.stopPropagation();
-            return;
-        }
-        this.touchid = event.getID();
+  protected OnClick() {}
 
-        this.onTouchStart(event);
+  protected onTouchStart(event: cc.Event.EventTouch) {}
+
+  protected onTouchMove(event: cc.Event.EventTouch) {}
+
+  protected onTouchEnd(event: cc.Event.EventTouch) {}
+
+  protected onTouchCancel(event: cc.Event.EventTouch) {}
+
+  protected onSetParent(perent: cc.Node) {}
+
+  private _onSetParent(parent: cc.Node) {
+    if (parent == null) {
+      this.touchid = null;
     }
 
-    private touchMove(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            //console.log(this.node.name, " touch move  touchid is different! ");
-            event.stopPropagation();
-            return;
-        }
-        this.touchid = event.getID();
-        this.onTouchMove(event);
-    }
-
-    private touchEnd(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            console.log(this.node.name, " touch end  touchid is different: ", this.touchid, event.getID());
-            event.stopPropagation();
-            return;
-        }
-
-        if (this.touchid == null) {
-            event.stopPropagation();
-            return;
-        }
-
-        this.touchid = null;
-        this.onTouchEnd(event);
-        this.OnClick();
-    }
-
-    private touchCancel(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            console.log(this.node.name, " touch cancel  touchid is different: ", this.touchid, event.getID());
-            event.stopPropagation();
-            return;
-        }
-
-        if (this.touchid == null) {
-            event.stopPropagation();
-            return;
-        }
-
-        this.touchid = null;
-        this.onTouchCancel(event);
-    }
-
-
-    protected OnClick() {
-
-    }
-
-    protected onTouchStart(event: cc.Event.EventTouch) {
-
-    }
-
-    protected onTouchMove(event: cc.Event.EventTouch) {
-
-    }
-
-    protected onTouchEnd(event: cc.Event.EventTouch) {
-
-
-    }
-
-    protected onTouchCancel(event: cc.Event.EventTouch) {
-
-    }
+    this.onSetParent(parent);
+  }
 }
--- Celer-Game/assets/Scripts/View/SingleTouchView.ts
+++ Celer-Game/assets/Scripts/View/SingleTouchView.ts
@@ -14,79 +14,70 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class SingleTouchView extends BaseView {
+  // LIFE-CYCLE CALLBACKS:
 
+  private touchid: number = null;
+  onLoad() {
+    this.node["_onSetParent"] = this._onSetParent.bind(this);
+    this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.touchCancel, this);
+    this.node.on(cc.Node.EventType.TOUCH_END, this.touchEnd, this);
+    this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMove, this);
+    this.node.on(cc.Node.EventType.TOUCH_START, this.touchStart, this);
+  }
 
-    // LIFE-CYCLE CALLBACKS:
+  start() {}
 
+  private touchStart(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      console.log(this.node.name, " touch start  touchid is different! ");
+      return;
+    }
+    this.touchid = event.getID();
+    this.onTouchStart(event);
+  }
 
-    private touchid: number = null;
-    onLoad() {
+  private touchMove(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      // console.log(this.node.name, " touch move  touchid is different! ");
+      return;
+    }
+    this.touchid = event.getID();
+    this.onTouchMove(event);
+  }
 
-        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.touchCancel, this);
-        this.node.on(cc.Node.EventType.TOUCH_END, this.touchEnd, this);
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMove, this);
-        this.node.on(cc.Node.EventType.TOUCH_START, this.touchStart, this);
+  private touchEnd(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      console.log(this.node.name, " touch end  touchid is different! ");
+      return;
+    }
+    this.touchid = null;
+    this.onTouchEnd(event);
+  }
 
+  private touchCancel(event: cc.Event.EventTouch) {
+    if (this.touchid !== null && this.touchid !== event.getID()) {
+      console.log(this.node.name, " touch cancel  touchid is different! ");
+      return;
+    }
+    this.touchid = null;
+    this.onTouchCancel(event);
+  }
+
+  protected onTouchStart(event: cc.Event.EventTouch) {}
+
+  protected onTouchMove(event: cc.Event.EventTouch) {}
+
+  protected onTouchEnd(event: cc.Event.EventTouch) {}
+
+  protected onTouchCancel(event: cc.Event.EventTouch) {}
+
+  protected onSetParent(perent: cc.Node) {}
+
+  private _onSetParent(parent: cc.Node) {
+    if (parent == null) {
+      this.touchid = null;
     }
 
-    start() {
-
-    }
-
-
-    private touchStart(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            console.log(this.node.name, " touch start  touchid is different! ");
-            return;
-        }
-        this.touchid = event.getID();
-        this.onTouchStart(event);
-    }
-
-    private touchMove(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            // console.log(this.node.name, " touch move  touchid is different! ");
-            return;
-        }
-        this.touchid = event.getID();
-        this.onTouchMove(event);
-    }
-
-    private touchEnd(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            console.log(this.node.name, " touch end  touchid is different! ");
-            return;
-        }
-        this.touchid = null;
-        this.onTouchEnd(event);
-    }
-
-    private touchCancel(event: cc.Event.EventTouch) {
-        if (this.touchid !== null && this.touchid !== event.getID()) {
-            console.log(this.node.name, " touch cancel  touchid is different! ");
-            return;
-        }
-        this.touchid = null;
-        this.onTouchCancel(event);
-    }
-
-
-    protected onTouchStart(event: cc.Event.EventTouch) {
-
-    }
-
-    protected onTouchMove(event: cc.Event.EventTouch) {
-
-    }
-
-    protected onTouchEnd(event: cc.Event.EventTouch) {
-
-
-    }
-
-    protected onTouchCancel(event: cc.Event.EventTouch) {
-
-    }
-
-    // update (dt) {}
+    this.onSetParent(parent);
+  }
 }
